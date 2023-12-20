a6191810f6cda20233aa35258d478a7305d6c538
Immanuel Valentino <imanuelv203@gmail.com>
2023-12-20 17:13:58 +0700

    auth delete

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import InputSearch from "./InputSearch"
-import UserActionButton from "./UserActionButton"
 
 const Navbar = () => {
     return (
@@ -8,7 +7,7 @@ const Navbar = () => {
             <div className="flex md:flex-row flex-col justify-between md:items-center p-5 gap-2"> 
                 <Link href="/" className="font-bold text-color-secondary text-2xl ">NUELANIMELIST</Link>
                 <InputSearch/>
-                <UserActionButton/>
+                
             </div>
         </header>
     )
