2e722d3f5a74e93008049983e1b0298c63b2b62b
Immanuel Valentino <imanuelv203@gmail.com>
2023-12-20 17:29:05 +0700

    changig nextconfigjs

--- next.config.js
+++ next.config.js
@@ -12,4 +12,15 @@ const nextConfig = {
     }
 }
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+  
+      if (!isServer) {
+        config.target = 'electron-renderer';
+        config.node = {
+          __dirname: true,
+        }
+      }
+      return config;
+    },
+  };
